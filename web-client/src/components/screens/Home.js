ec4f0de8f4f524f7e6bd13c039b21e14add1c21e
bmaansi <bhardwajmaansi@gmail.com>
2024-02-28 12:52:57 -0800

    Added to the home page

--- web-client/src/components/screens/Home.js
+++ web-client/src/components/screens/Home.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Home = ()=>{
     return(
-        <h1>Home</h1>
+        <div className='home'>
+            <div className='card home-card'>
+                <h5>Username</h5>
+                <div className='card-image'>
+                    <img src='https://plus.unsplash.com/premium_photo-1681826664053-5f50e4a0c513?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c21vb3RoaWVzfGVufDB8fDB8fHww'/>
+                </div>
+                <div className='card-content'>
+                <i class="material-icons">favorite_border</i>
+                <i class="material-icons">bookmark_border</i>
+                    <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                        <h6>Username</h6> 
+                        <h6>MM/DD/YYYY</h6>
+                        
+                    </div>              
+                    <p>This is the caption...</p>
+                    <input type="text" placeholder='Add comment'/>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
--- web-client/src/components/screens/Profile.js
+++ web-client/src/components/screens/Profile.js
@@ -6,7 +6,7 @@ const Profile = ()=>{
             <div style={{display:"flex", justifyContent:"space-around",margin:"18px 0px", borderBottom:"1px solid grey"}}>
                 <div>
                     <img style={{width:"160px", height:"160px", borderRadius:"80px"}}
-                    src='https://images.unsplash.com/photo-1682686581220-689c34afb6ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                    src='https://images.unsplash.com/photo-1698358792121-407062f81a49?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
                     />
                 </div>
                 <div>
