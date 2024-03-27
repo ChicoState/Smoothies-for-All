586ec8b854d1026271c307120e6d215cc244bf77
bmaansi <bhardwajmaansi@gmail.com>
2024-03-27 14:43:51 -0700

    Implimented Logout

--- web-client/src/components/screens/Home.js
+++ web-client/src/components/screens/Home.js
@@ -19,20 +19,14 @@ const Home = ()=>{
                 data.map(item=>{
                     return(
                         <div className='card home-card' key={item._id}>
-                           {/* <h5>{item.postedBy.username}</h5> */}
-                           <h5>username</h5>
-
+                           <h5>{item.postedBy.username}</h5>
                            <div className='card-image'>
                                <img src={item.photo}/>
                            </div>
                            <div className='card-content'>
                            <i class="material-icons">favorite_border</i>
                            <i class="material-icons">bookmark_border</i>
-                               {/* <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-                                   { <h6>{item.postedBy.name}</h6> 
-                                   <h6>MM/DD/YYYY</h6> }
-                                   
-                               </div>   */}
+                               
                                <h6>{item.title}</h6>          
                                <p>{item.body}</p>
                                <input type="text" placeholder='Add comment'/>
